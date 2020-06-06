2e369642577a003f773457141765a548d846e373
yana-selezneva02 <58134266+yana-selezneva02@users.noreply.github.com>
2020-06-06 16:39:44 +0700

    my first commit

---
+++ Cinema/Cinema/Models/FilmContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+
+namespace Cinema.Models
+{
+    public class FilmContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+        internal static void AddRange(Film film1, Film film2, Film film3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
